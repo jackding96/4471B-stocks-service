cb069a7b9a29956892ddef6f8f3bfec6b7d53f58
jackding96 <jackding96@outlook.com>
2019-03-11 18:27:26 -0400

    remove tarball

--- sub.js
+++ sub.js
@@ -1,8 +1,8 @@
 const redis = require('redis');
 
 const options = {port: 8080};
-sub = redis.createClient(options);
 
+sub = redis.createClient(options);
 sub.subscribe("stocks");
 
 sub.on("message", function (channel, message) {
