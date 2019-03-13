c8f6a44257d475216f3b31ede60c42484b8c71a8
jackding96 <jackding96@outlook.com>
2019-03-13 03:40:50 -0400

    live updates for MSFT working

--- publisher.js
+++ publisher.js
@@ -1,17 +1,21 @@
 const rp = require('request-promise');
 
 var admin = require("firebase-admin");
-
-var serviceAccount = require("firebase-credentials.json");
+var serviceAccount = require("./firebase-credentials.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://cs4471-architecture.firebaseio.com"
 });
 
+const db = admin.firestore();
+var docRef = db.collection('services').doc('stocks');
+
 function fetchStocks(){
   return rp('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=demo')
-    .then(res => res)
+    .then(res => {
+      docRef.set(JSON.parse(res));
+    })
     .catch(err => err);
 }
 
